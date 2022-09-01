b31b5bb85c2f075ac2b26455af55f8f295673913
taylorcovington <tcovingtondev@gmail.com>
2022-09-01 16:16:37 -0600

    update to logo

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "E-Comm Design Studio",
+  title: "Ecom Dev Studio",
   viewport: "width=device-width,initial-scale=1",
 });
 
